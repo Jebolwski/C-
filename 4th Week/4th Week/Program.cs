80283a1c9aa53b99d5f3b442a4f7dbb8fcd53c6a
Mert Gökmen <88985262+Jebolwski@users.noreply.github.com>
2022-10-28 14:11:21 +0300

    Add files via upload

---
+++ 4th Week/4th Week/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+
+namespace Program
+{
+    class Sample
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Foo(15,25));
+            Console.WriteLine(isPrime(4));
+            asalCarpanlar(1270);
+            Console.WriteLine("\n****************");
+            int n = 100;
+            int i = 2;
+            while (n != 1)
+            {
+                if (n % i == 0)
+                {
+                    Console.Write("{0} ", i);
+                    n /= i;
+                }
+                else
+                    i++;
+            }
+        }
+
+        public static int Foo(int a=10,int b=20,int c=30)
+        {
+            Console.WriteLine("Foo()...");
+            return a + b + c;
+        }
+
+        public static bool isPrime(int j)
+        {
+            for(int i = 2; i <= Math.Sqrt(j); i++)
+            {
+                if (j % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static void asalCarpanlar(int j)
+        {
+            for (int i = 2; i <=j/2; i++)
+            {
+                if ( isPrime(i) && j % i == 0)
+                {
+                    Console.Write(i+" ");
+                }
+            }
+        }
+
+
+    }
+}
