6462caafc3b97a9d4aae2f75fd9f772ff38b0891
Mert Gökmen <88985262+Jebolwski@users.noreply.github.com>
2022-11-30 19:40:17 +0300

    Add files via upload

--- Work1/Program.cs
+++ Work1/Program.cs
@@ -28,10 +28,58 @@ namespace Program
             messi.Phsicality = 76;
             //messi.ShootGoal();
 
-            while (!Console.KeyAvailable)
+            //while (!Console.KeyAvailable)
+            //{
+            //    DateTime t = DateTime.Now;
+            //    Console.Write("{0:D2}:{1:D2}:{2:D2}\r",t.Hour, t.Minute, t.Second);
+            //}
+
+            int[,] a = new int[9, 9]
             {
-                DateTime t = DateTime.Now;
-                Console.Write("{0:D2}:{1:D2}:{2:D2}\r",t.Hour, t.Minute, t.Second);
+                {5, 3, 4, 6, 7, 8, 9, 1, 2 },
+                {6, 7, 2, 1, 9, 5, 3, 4, 8 },
+                { 1, 9, 8, 3, 4, 2, 5, 6, 7},
+                {8, 5, 9, 7, 6, 1, 4, 5, 3 },
+                {4, 2, 6, 8, 5, 3, 7, 9, 1},
+                {7, 1, 3, 9, 2, 4, 8, 5, 6 },
+                {9, 6, 1, 5, 3, 7, 2, 8, 4 },
+                {2, 8, 7, 4, 1, 9, 6, 3, 5},
+                {3, 4, 5, 2, 8, 6, 1, 7, 9 }
+            };
+
+
+            for(int i = 0; i < 9; i++)
+            {
+                
+                int[] array = new int[9];
+                //Getting 1d sinlge array
+                for (int j=0;j<9;j++)
+                {
+                    array[j] = a[i,j];
+                }
+                int min_index = 0;
+                for (int k = 0; k < 9; k++)
+                {
+                    min_index = k;
+                    for (int b = k; b < 9; b++)
+                    {
+                        if (array[b] < array[min_index])
+                        {
+                            min_index = b;
+                        }
+                    }
+                    int temp = array[min_index];
+                    array[min_index] = array[k];
+                    array[k] = temp;
+                }
+                for(int j=0; j<8; j++)
+                {
+                    if(array[j]+1 != array[j + 1])
+                    {
+                        Console.WriteLine("Olmaz {0}. satırda hata var.",i);
+                        return;
+                    }
+                }
             }
 
         }
