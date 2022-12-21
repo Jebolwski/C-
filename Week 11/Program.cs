92c82d7ae1b6a52646763ba880ed4f8284311b93
Mert Gökmen <88985262+Jebolwski@users.noreply.github.com>
2022-12-21 11:55:48 +0300

    Add files via upload

--- Week 11/Program.cs
+++ Week 11/Program.cs
@@ -56,6 +56,30 @@ namespace Program
             Foo(tur);
             Console.WriteLine("===============");
 
+            object obje;
+            int num = 214;
+            double longnum;
+            
+            //Kutulama
+            obje= (object)num;
+            longnum= (int)obje;
+            Console.WriteLine(longnum);
+            Console.WriteLine("=================");
+            ArrayList arrayList= new ArrayList();
+            
+            arrayList.Add(num);
+            arrayList.Add(longnum);
+            arrayList.Add(obje);
+
+            foreach(object item in arrayList)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("messi");
+            Kutuphane kutuphane = new Kutuphane(2019,"Millet Kütüphanesi",198);
+            Console.WriteLine("ronaldo");
+            Console.WriteLine(kutuphane.ToString());
+            
         }
         public static void Foo(object o)
 
@@ -147,4 +171,55 @@ namespace Program
 
     }
 
+    class Person
+    {
+        public string name;
+        public int age;
+
+        public Person() { }
+
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }   
+    }
+
+    class Kutuphane
+    {
+        private int creationdate;
+        private string name;
+        private int capacity;
+        public int creation_date
+        {
+            get { return creationdate; }
+            set { creationdate = value > 500 ? value : -1; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Capacity
+        {
+            get { return capacity; }
+            set { capacity = value > 5 ? value : -1; }
+        }
+
+        public Kutuphane(int creation_date, string name, int capacity)
+        {
+            this.creation_date = creation_date;
+            this.Name = name;
+            this.Capacity = capacity;
+        }
+
+        public Kutuphane() { }
+
+        public override string ToString()
+        {
+            return this.Name+"-"+this.creation_date+"-"+this.Capacity;
+        }
+    }
 }
