8a3e4287dbe3cc25735ed1fbaac4e41318bf73a7
Mert Gökmen <88985262+Jebolwski@users.noreply.github.com>
2024-04-05 15:42:24 +0300

    Add files via upload

---
+++ Patika/Patika/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Proc
+{
+    class Program
+    {
+
+        static void Main()
+        {
+            int x = 12;
+            string y = "adana";
+            bool c = true;
+
+            Console.WriteLine(x+" "+y+" "+c);
+
+        }
+    }
+}
